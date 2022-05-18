4adb6c5ccd95732b0e270d261f2e949c1b41997e
MrPolymorph <{ID}+{username}@users.noreply.github.com>
2022-05-18 13:50:26 +0100

    parking progress

--- src/overlays/actors/ovl_Obj_Tokei_Tobira/z_obj_tokei_tobira.c
+++ src/overlays/actors/ovl_Obj_Tokei_Tobira/z_obj_tokei_tobira.c
@@ -5,7 +5,7 @@
  */
 
 #include "z_obj_tokei_tobira.h"
-
+#include "objects/object_tokei_tobira/object_tokei_tobira.h"
 #define FLAGS 0x00000000
 
 #define THIS ((ObjTokeiTobira*)thisx)
@@ -14,8 +14,9 @@ void ObjTokeiTobira_Init(Actor* thisx, GlobalContext* globalCtx);
 void ObjTokeiTobira_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ObjTokeiTobira_Update(Actor* thisx, GlobalContext* globalCtx);
 void ObjTokeiTobira_Draw(Actor* thisx, GlobalContext* globalCtx);
+void func_80ABD3B0(ObjTokeiTobira* thisx, GlobalContext* globalCtx);
 
-#if 0
+
 const ActorInit Obj_Tokei_Tobira_InitVars = {
     ACTOR_OBJ_TOKEI_TOBIRA,
     ACTORCAT_BG,
@@ -28,7 +29,6 @@ const ActorInit Obj_Tokei_Tobira_InitVars = {
     (ActorFunc)ObjTokeiTobira_Draw,
 };
 
-// static InitChainEntry sInitChain[] = {
 static InitChainEntry D_80ABD750[] = {
     ICHAIN_F32(uncullZoneForward, 4000, ICHAIN_CONTINUE),
     ICHAIN_F32(uncullZoneScale, 300, ICHAIN_CONTINUE),
@@ -36,16 +36,62 @@ static InitChainEntry D_80ABD750[] = {
     ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
 };
 
-#endif
+extern CollisionHeader *D_80ABD770[];
+extern s16 D_80ABD76C;
+extern Vec3f D_80ABD760;
+extern Gfx *D_80ABD780[];
 
-extern InitChainEntry D_80ABD750[];
+void ObjTokeiTobira_Init(Actor* thisx, GlobalContext* globalCtx) {
+    ObjTokeiTobira* this = THIS;
+    s16* sp44;
+    s32 params;
+    Vec3f sp48;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Tokei_Tobira/ObjTokeiTobira_Init.s")
+    params = this->dyna.actor.params & 1;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Tokei_Tobira/ObjTokeiTobira_Destroy.s")
+    Actor_ProcessInitChain(&this->dyna.actor, D_80ABD750);
+    DynaPolyActor_Init(&this->dyna, 3);
+    DynaPolyActor_LoadMesh(globalCtx, &this->dyna, D_80ABD770[params]);
+    if (params == 0) {
+        Actor_SpawnAsChild(&globalCtx->actorCtx, &this->dyna.actor, globalCtx, ACTOR_OBJ_TOKEI_TOBIRA, this->dyna.actor.world.pos.x,
+                           this->dyna.actor.world.pos.y, this->dyna.actor.world.pos.z, this->dyna.actor.shape.rot.x, this->dyna.actor.shape.rot.y,
+                           this->dyna.actor.shape.rot.z,1);
+    }
+    sp44 = (s32)((params * 1) + &D_80ABD76C);
+    Matrix_RotateY(this->dyna.actor.shape.rot.y + *sp44, 0);
+    Matrix_MultiplyVector3fByState(&D_80ABD760, &sp48);
+    this->dyna.actor.world.pos.x += sp48.x;
+    this->dyna.actor.world.pos.y += sp48.y;
+    this->dyna.actor.world.pos.z += sp48.z;
+    if (sp44 == &D_80ABD76C && (gSaveContext.save.weekEventReg[59] & 4) == 0 && globalCtx->sceneNum == SCENE_CLOCKTOWER && gSaveContext.sceneSetupIndex == 0 && this->dyna.actor.cutscene >= 0) {
+        this->actionFunc = func_80ABD3B0;
+        this->dyna.actor.flags |= 0x10;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Tokei_Tobira/func_80ABD3B0.s")
+void ObjTokeiTobira_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    ObjTokeiTobira* this = (ObjTokeiTobira* ) thisx;
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
+}
+
+void func_80ABD3B0(ObjTokeiTobira* this, GlobalContext* globalCtx) {
+    s16 cutscene;
+
+    cutscene = this->dyna.actor.cutscene;
+
+    if (ActorCutscene_GetCanPlayNext(cutscene) != 0) {
+        ActorCutscene_StartAndSetUnkLinkFields(this->dyna.actor.cutscene, &this->dyna.actor);
+        gSaveContext.save.weekEventReg[0x3B] |= 4;
+        this->actionFunc = 0;
+        this->dyna.actor.flags &= -0x11;
+        return;
+    }
+    ActorCutscene_SetIntentToPlay(this->dyna.actor.cutscene);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Tokei_Tobira/ObjTokeiTobira_Update.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Tokei_Tobira/ObjTokeiTobira_Draw.s")
+void ObjTokeiTobira_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    ObjTokeiTobira* this = (ObjTokeiTobira* ) thisx;
+    Gfx_DrawDListOpa(globalCtx, D_80ABD780[PARAMS(thisx)]);
+}
--- src/overlays/actors/ovl_Obj_Tokei_Tobira/z_obj_tokei_tobira.h
+++ src/overlays/actors/ovl_Obj_Tokei_Tobira/z_obj_tokei_tobira.h
@@ -8,12 +8,13 @@ struct ObjTokeiTobira;
 typedef void (*ObjTokeiTobiraActionFunc)(struct ObjTokeiTobira*, GlobalContext*);
 
 typedef struct ObjTokeiTobira {
-    /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x18];
+    /* 0x0000 */ DynaPolyActor dyna;
     /* 0x015C */ ObjTokeiTobiraActionFunc actionFunc;
     /* 0x0160 */ char unk_160[0x10];
 } ObjTokeiTobira; // size = 0x170
 
 extern const ActorInit Obj_Tokei_Tobira_InitVars;
+
+#define PARAMS(thisx) ((thisx)->params & 3)
 
 #endif // Z_OBJ_TOKEI_TOBIRA_H
